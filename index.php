69c7e45a493f48b371ff80ece4c9c8144e9b0dda
neama-ahmad <neama.ahmad22@gmail.com>
2022-02-20 21:35:35 +0300

    add files

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="ar">
+
+<head>
+  <meta charset="UTF8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="content-type" content="text/html;charset=UTF-8">
+  <link href="https://fonts.googleapis.com/css2?family=Tajawal&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.0/css/all.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.0/css/all.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.0/css/all.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+ 
+  <link rel="icon" href="img/logo.png">
+  <title>سداد مكة لسداد القروض</title>
+  <link rel="stylesheet" href="css/boxes.css">
+  <link rel="stylesheet" href="css/main.css">
+  
+</head>
+
+<body  onload="document.body.style.opacity='1' ">
+  
+  <!--menu-->
+  <header class="header">
+    <a href="index.php"><img src="img/logocrop.png"></a>
+    <input class="menu-btn" type="checkbox" id="menu-btn" />
+    <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+    <ul class="menu">
+    <li class="menu-choice social-top"><a href="#contact" >تواصل معنا</a></li>
+    </ul>
+  </header>
+
+  <!--cover-->
+  <div class="cover">
+    <img src="https://i.pinimg.com/originals/f8/c4/22/f8c422a0a0e6793b3f9113d419c5143a.gif">
+   
+    <h1 class="bigTitle">سداد مكة لسداد القروض 0591778709 </h1>
+    <p class="inTitle">تواصل معنا وراح نساعدك في تسديد قروضك البنكية أو في تسديد إيقاف الخدمات والتعثرات البنكية والقروض القديمة وكمان في استخراج قروض جديدة ونضمن لك السرعة في الأداء وأسعار مميزة</p>
+
+    <a href="#contact" class="button">تواصل معنا</a>
+  </div>
+
+  <!--services-->
+  <div id="services">
+    <h2 class="Title" >خدماتنا</h2>
+
+    <div class="row">
+      <div class="column">
+        <i class="fas fa-hand-holding-usd  fa-3x icons"></i>
+        <p class="features">استخراج قرض جديد لدى جميع البنوك</p>
+      </div>
+
+      <div class="column">
+        <i class="fab fa-cc-visa fa-3x icons"></i>
+        <p class="features">تسديد قروض  بنكية</p>
+      </div>
+
+      <div class="column">
+        <i class="fas fa-search-dollar fa-3x icons"></i>
+        <p class="features">سداد المتعثرات لدى سمه</p>
+      </div>
+
+      <div class="column">
+        <i class="fas fa-gavel fa-3x icons"></i>
+        <p class="features">سداد إيقاف الخدمات لدى المحاكم</p>
+      </div>
+
+      <div class="column">
+        <i class="fas fa-university fa-3x icons"></i>
+        <p class="features">التحويل من بنك إلى بنك آخر</p>
+      </div>
+
+      <div class="column">
+        <i class="fas fa-home fa-3x icons"></i>
+        <p class="features">توفير دفعه و ضريبة للقرض العقاري لنزول القرض</p>
+      </div>
+
+    </div>
+  </div>
+
+
+
+  <!--contact-->
+  <div id ="contact">
+    <h2 class="Title">تواصل معنا</h2>
+    <!--social media buttons-->
+    <div class="banner" id="banner1">
+      <a href= "mailto:humud505@gmail.com" class="far fa-envelope email"></a>
+      <a href="https://wa.me/966591778709" target="_blank" class="fa fa-whatsapp"></a>
+      <a href="https://www.facebook.com/profile.php?id=100070357513607" target="_blank" class="fa fa-facebook"></a>
+      <a href="https://twitter.com/sadadmakkah" target="_blank" class="fa fa-twitter"></a>
+    </div>
+
+  </div>
+
+  <script async data-id="79451" src="https://cdn.widgetwhats.com/script.min.js"></script>
+
+  <!--client-->
+  <div class="info" id="client">
+    <div class="imgwrapper">
+      <div class="imgAd"><img src="img/ahly.png"></div>
+      <div class="imgAd2"><img src="img/rajihi.png"></div>
+      <div class="imgAd"><img src="img/ryadh.png"></div>
+    </div>
+  </div>
+  
+  <!--footer-->
+  <div  class="copyright">
+    <p> جميع الحقوق محفوظه &copy; <span id="year"></span>&nbsp;<a href="index.php">لسداد مكة</a></p>
+    <script>document.getElementById("year").innerHTML=(new Date().getFullYear())</script>
+  </div>
+
+
+</body>
+
+</html>
